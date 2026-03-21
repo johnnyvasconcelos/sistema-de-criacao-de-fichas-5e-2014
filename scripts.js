0444f80cca0a8ad8016117d2498f4b15629ca6b4
Jhonatan <jvasconcelosbittencourt@gmail.com>
2026-03-21 12:42:07 -0300

    atribuindo raças

--- scripts.js
+++ scripts.js
@@ -1,18 +1,36 @@
 {
-  const dados = {};
+  const data = {};
   const steps = document.querySelectorAll(".step");
   const inputs = document.querySelectorAll(".etapa");
+  const exoticRaces = document.querySelectorAll('option[data-tipo="exotica"]');
+  const activeRaces = document.querySelector("#ativa-racas");
+  // mostra ou esconde as raças fora do ldj
+  for (let i = 0; i < exoticRaces.length; i++) {
+    exoticRaces[i].hidden = true;
+  }
+  activeRaces.addEventListener("change", (ev) => {
+    if (ev.target.checked) {
+      for (let i = 0; i < exoticRaces.length; i++) {
+        exoticRaces[i].hidden = false;
+      }
+    } else {
+      for (let i = 0; i < exoticRaces.length; i++) {
+        exoticRaces[i].hidden = true;
+      }
+    }
+  });
+  // envia as opções selecionadas para o data
   for (let i = 0; i < inputs.length; i++) {
     inputs[i].addEventListener("change", (ev) => {
       const dado = ev.target.name;
-      dados[dado] = ev.target.value;
+      data[dado] = ev.target.value;
       const atualStep = ev.target.closest(".step");
       const index = Array.from(steps).indexOf(atualStep);
       if (steps[index + 1]) {
         steps[index].classList.remove("on");
         steps[index + 1].classList.add("on");
       }
-      console.log(dados);
+      console.log(data);
     });
   }
 }
